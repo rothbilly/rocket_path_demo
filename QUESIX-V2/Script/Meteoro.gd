387da80a576319a068d29acef5d9c15c6b1eed03
rothbilly <roth81ly@gmail.com>
2023-11-28 19:48:27 -0300

    updated rocket path project and godot 4

--- QUESIX-V2/Script/Meteoro.gd
+++ QUESIX-V2/Script/Meteoro.gd
@@ -3,11 +3,11 @@ class_name Meteor
 
 var initialrotation 
 
-onready var animation = $AnimationPlayer
+@onready var animation = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initialrotation = rand_range(0, 360)
-	$Sprite.rotation_degrees = initialrotation
+	initialrotation = randf_range(0, 360)
+	$Sprite2D.rotation_degrees = initialrotation
 	animation.play("Rotate")
 
